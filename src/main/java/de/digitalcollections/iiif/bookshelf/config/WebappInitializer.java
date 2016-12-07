c7a7038d910048c6570b1c1d0154674932cde487
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2016-12-07 15:49:27 +0100

    Added logging of the current context.

--- src/main/java/de/digitalcollections/iiif/bookshelf/config/WebappInitializer.java
+++ src/main/java/de/digitalcollections/iiif/bookshelf/config/WebappInitializer.java
@@ -3,11 +3,15 @@ package de.digitalcollections.iiif.bookshelf.config;
 import ch.qos.logback.ext.spring.web.LogbackConfigListener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Order(2)
 public class WebappInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(WebappInitializer.class);
 
   @Override
   protected Class<?>[] getRootConfigClasses() {
@@ -28,5 +32,8 @@ public class WebappInitializer extends AbstractAnnotationConfigDispatcherServlet
   public void onStartup(ServletContext servletContext) throws ServletException {
     super.onStartup(servletContext);
     servletContext.addListener(new LogbackConfigListener());
+
+    String contextPath = servletContext.getContextPath();
+    LOGGER.info("*** Deployed under context path '{}'", contextPath);
   }
 }
