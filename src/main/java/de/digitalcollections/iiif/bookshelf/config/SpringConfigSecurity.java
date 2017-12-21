7fdc20c31762fbb5919a2966916ea88997063a7f
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-12-21 16:47:55 +0100

    Fix uv and login posts combination...

--- src/main/java/de/digitalcollections/iiif/bookshelf/config/SpringConfigSecurity.java
+++ src/main/java/de/digitalcollections/iiif/bookshelf/config/SpringConfigSecurity.java
@@ -67,22 +67,19 @@ public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
       if (!authentication) {
         return;
       }
-      http
-              .antMatcher("/add").authorizeRequests()
-              .antMatchers("/add").authenticated()
+      http.antMatcher("/add").authorizeRequests().antMatchers("/add").authenticated()
               .and()
-              .formLogin()
-              .loginPage("/login");
+              .formLogin().loginPage("/login").permitAll().and().httpBasic();
     }
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.antMatcher("/uv").headers().frameOptions().disable(); // to make universalviewer work
+    http.authorizeRequests().anyRequest().permitAll()
+            .and().formLogin().loginPage("/login").and().httpBasic()
+            .and().headers().frameOptions().disable(); // to make universalviewer work
     /* Refused to display 'http://localhost:8080/webjars/universalviewer/2.0.2/dist/uv-2.0.2/app.html?isHomeDomain=true&isOnlyInstance=true&manifestUri=https%3A%2F%2Fapi.digitale-sammlungen.de%2Fiiif%2Fpresentation%2Fv2%2Fbsb00010484_00505_u001%2Fmanifest&embedScriptUri=http://localhost:8080/webjars/universalviewer/2.0.2/dist/uv-2.0.2/lib/embed.js&embedDomain=localhost&domain=localhost&isLightbox=false&locale=en-GB&xdm_e=http%3A%2F%2Flocalhost%3A8080%2Fuv%2F1FC1F766&xdm_c=default127&xdm_p=4' in a frame because it set 'X-Frame-Options' to 'deny'.
      */
-
-    http.authorizeRequests().anyRequest().permitAll().and().formLogin().loginPage("/login").and().httpBasic();
 
     // and this line is needed so that http basic authentication with configured username and password (in application.yml) is working again
 //    http.authorizeRequests().antMatchers("/monitoring").authenticated().and().httpBasic();
